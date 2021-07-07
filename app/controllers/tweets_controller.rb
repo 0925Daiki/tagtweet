b8f9e05a9ea161752a062c129b1c1c04296ba443
endodaiki <82555115+0925Daiki@users.noreply.github.com>
2021-07-08 07:21:36 +0900

    controller(create)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,5 +7,21 @@ class TweetsController < ApplicationController
   def new
     @tweet = TweetTag.new
   end
-  
+
+  def create
+    @tweet = TweetsTag.new(tweet_params)
+    if @tweet.valid?
+      @tweet.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweets_tag).parmit(:message, :name)
+  end
+
 end
